3fb5e1f41c2de2e91e18bb2029a225e861ff3b98
SharvariKarmalkar <sharvarikarmalkar03@gmail.com>
2026-03-12 12:56:27 +0530

    Add full internship portal project

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,28 @@
+// // lib/db.js
+// import pkg from "pg";
+
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "internship_portal",
+//   password: "admin",
+//   port: 5432,
+// });
+
+// export { pool };
+
 // lib/db.js
 import pkg from "pg";
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "internship_portal",
-  password: "admin",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export { pool };
